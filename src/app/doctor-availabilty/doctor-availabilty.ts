90dffab1d6302cb3baf59988260f05bbbd6aa815
Sashank Patnaik <sashankpatnaik9407@gmail.com>
2025-09-09 13:10:38 +0530

    First Commit

---
+++ src/app/doctor-availabilty/doctor-availabilty.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-availabilty',
+  imports: [],
+  templateUrl: './doctor-availabilty.html',
+  styleUrl: './doctor-availabilty.css'
+})
+export class DoctorAvailabilty {
+
+}
